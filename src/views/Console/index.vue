2a318eb6526f1769361a257ae1d3d2d56becb857
rookie996-object <472099249@qq.com>
2021-03-04 23:44:07 +0800

    refactor: index

--- src/views/Console/index.vue
+++ src/views/Console/index.vue
@@ -1,29 +1,29 @@
 <template>
-  <div>控制台</div>
+  <div class="wrapper">
+    <el-image style="width: 100px; height: 100px" :src="url" fit></el-image>
+    <h2>欢迎进入汽车租赁管理后台</h2>
+  </div>
 </template>
-
 <script>
-import { reactive, ref, onMounted, watch } from "@vue/composition-api";
 export default {
-  name: "ConsoleIndex",
-  components: {},
-  props: {},
-  setup(props, { root }){
-    // 监听
-    // watch(xxx, () => {})
-    // 生命周期
-    // onBeforeMount(() => {})
-    // onMounted(() => {})
-    // onBeforeUpdate(() => {})
-    // onUpdated(() => {})
-    // onBeforeUnmount(() => {})
-    // onUnmounted(() => {})
-    // onErrorCaptured(() => {})
-    // onRenderTracked(() => {})
-    // onRenderTriggered(() => {})
-    return {}
-  }
-}
+  data() {
+    return {
+      fit: ["fill"],
+      url:
+        "https://fuss10.elemecdn.com/e/5d/4a731a90594a4af544c0c25941171jpeg.jpeg",
+    };
+  },
+};
 </script>
 <style lang="scss" scoped>
+.wrapper {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  margin: 200px;
+  h2 {
+    font-size: 36px;
+    font-weight: 600;
+  }
+}
 </style>
