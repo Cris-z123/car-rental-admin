c83cb61b448139392a2de3d0945c380c2626a4b8
rookie996-object <472099249@qq.com>
2021-03-07 17:02:27 +0800

    style: rouer titl and tip message

--- src/router/title.js
+++ src/router/title.js
@@ -2,7 +2,7 @@ import router from "./index";
 router.beforeEach((to, from, next) => {
     /* 路由发生变化修改页面title */
     if (to.meta.title) {
-        document.title = to.meta.title
+        document.title = "汽车租赁管理平台-" + to.meta.title
     }
     next()
 })
